734c108207ddd4b394204ea95e4696dcbc1d73d1
Victor <victor.oteroroldan@gmail.com>
2021-04-10 22:00:39 +0200

    first commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,11 +11,18 @@ const Map = (props) => {
     return (
         <div>
             <GoogleMap 
-            defaultZoom={14}
-            defaultCenter={{ lat:41.402, lng: 2.174}}
+            defaultZoom={15}
+            defaultCenter={{ lat:41.402, lng: 2.176}}
             />
             
-            <Marker position={{lat:41.402,lng:2.174}}
+            <Marker 
+            position={{lat:41.400,lng:2.174}} 
+            icon="https://i.postimg.cc/WzJY3GYY/circle-1.png"
+            
+            
+            />
+             <Marker 
+             position={{lat:41.407,lng:2.180}} 
             
             
             />
